e57da2931ce539840093c95be25a9ade7f6f6bbe
Matt Goldspink <mattgoldspink1@gmail.com>
2021-03-04 20:38:21 +0000

    Trialling new AR button

--- mixesdb_essential_mix_importer.user.js
+++ mixesdb_essential_mix_importer.user.js
@@ -3,11 +3,12 @@
 // @author         mattgoldspink
 // @namespace      https://github.com/mattgoldspink/musicbrainz-userscripts/
 // @description    One-click importing of releases from beatport.com/release pages into MusicBrainz
-// @version        2022.03.03.4
+// @version        2022.03.04.1
 // @downloadURL    https://github.com/mattgoldspink/musicbrainz-userscripts/raw/mgoldspink/feature_mixesdb/mixesdb_essential_mix_importer.user.js
 // @updateURL      https://github.com/mattgoldspink/musicbrainz-userscripts/raw/mgoldspink/feature_mixesdb/mixesdb_essential_mix_importer.user.js
 // @include        http://www.mixesdb.com/w/*
 // @include        https://www.mixesdb.com/w/*
+// @include        https://musicbrainz.org/release/*/edit-relationships
 // @require        https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @require        lib/mbimport.js
 // @require        lib/logger.js
@@ -25,8 +26,12 @@ $(document).ready(function () {
     MBImportStyle();
 
     let release_url = window.location.href.replace('/?.*$/', '').replace(/#.*$/, '');
-    let release = retrieveReleaseInfo(release_url);
-    insertLink(release, release_url);
+    if (/mixesdb/.test(window.location.hostname)) {
+        let release = retrieveReleaseInfo(release_url);
+        insertLink(release, release_url);
+    } else if (/musicbrainz/.test(window.location.hostname)) {
+        insertARLink();
+    }
 });
 
 function retrieveReleaseInfo(release_url) {
@@ -140,4 +145,83 @@ function insertLink(release, release_url) {
     $('#mw-content-text').prepend(mbUI);
     $('form.musicbrainz_import').css({ display: 'inline-block', 'margin-left': '5px' });
     mbUI.slideDown();
+}
+
+function insertARLink() {
+    const button = document.createElement('button');
+    button.innerText = 'Add Essential mix ARs';
+    button.addEventListener(
+        'click',
+        () => {
+            performARUpdate();
+        },
+        false
+    );
+    $('#batch-tools').before(button);
+}
+
+function performARUpdate() {
+    $('.subheader>a').forEach(function () {
+        const artistURL = this.href;
+
+        $('#release-rels .add-rel')[0].dispatchEvent(makeClickEvent());
+
+        const type = $('.ui-dialog .link-type')[0];
+        type.value = 43;
+        type.dispatchEvent(makeChangeEvent());
+
+        const input = $('.name.ui-autocomplete-input')[0];
+
+        input.dispatchEvent(makeClickEvent());
+        input.value = artistURL;
+        input.dispatchEvent(makeKeyDownEvent(13));
+
+        $('.ui-dialog .positive').click();
+    });
+
+    $('release-group-rels .add-rel');
+
+    const groupType = $('.ui-dialog .release-group-rels')[0];
+    groupType.value = 'series';
+    groupType.dispatchEvent(makeChangeEvent());
+
+    const input = $('.name.ui-autocomplete-input')[0];
+
+    input.dispatchEvent(makeClickEvent());
+    input.value = 'https://musicbrainz.org/series/10efa767-57d6-404a-abfb-47a3d8fef520';
+    input.dispatchEvent(makeKeyDownEvent(13));
+
+    $('.ui-dialog .positive').click();
+
+    $('.submit.positive').click();
+}
+
+function makeClickEvent() {
+    const evt = document.createEvent('HTMLEvents');
+    evt.initEvent('click', true, true);
+    return evt;
+}
+
+function makeChangeEvent() {
+    const evt = document.createEvent('HTMLEvents');
+    evt.initEvent('change', false, true);
+    return evt;
+}
+
+function makeKeyDownEvent(keyCode) {
+    const keyboardEvent = document.createEvent('KeyboardEvent');
+    const initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? 'initKeyboardEvent' : 'initKeyEvent';
+    keyboardEvent[initMethod](
+        'keydown', // event type: keydown, keyup, keypress
+        true, // bubbles
+        true, // cancelable
+        window, // view: should be window
+        false, // ctrlKey
+        false, // altKey
+        false, // shiftKey
+        false, // metaKey
+        keyCode, // keyCode: unsigned long - the virtual key code, else 0
+        0 // charCode: unsigned long - the Unicode character associated with the depressed key, else 0
+    );
+    return keyboardEvent;
 }
